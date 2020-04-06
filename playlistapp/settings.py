ed4de0e78fbc51d473291cc900e669f5ccba1ee3
escribapetrus <schreiber.mmb@gmail.com>
2020-04-06 20:23:27 -0300

    sdjkf

--- playlistapp/settings.py
+++ playlistapp/settings.py
@@ -77,7 +77,7 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-STATIC_ROOT = os.path.join(BASE_DIR)
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static/")]
 STATIC_URL = '/static/'
 
