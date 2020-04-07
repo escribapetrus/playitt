afc66053a46dbdaee51bc91bc4597d2e497a4a97
escribapetrus <schreiber.mmb@gmail.com>
2020-04-06 21:47:22 -0300

    kfsdfkjsf

--- playlistapp/settings.py
+++ playlistapp/settings.py
@@ -77,8 +77,8 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-#STATIC_ROOT = os.path.join(BASE_DIR,"static")
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),'/var/www/static/',]
+STATIC_ROOT = os.path.join(BASE_DIR,"static")
+STATICFILES_DIRS = ['/var/www/static/']
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/playlists'
