8f91f8cb1df4a2b203cfebe06ae77dfa9f8e41ba
escribapetrus <schreiber.mmb@gmail.com>
2020-09-17 16:18:54 -0300

    collected static files

--- _main/settings.py
+++ _main/settings.py
@@ -98,8 +98,8 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-STATIC_ROOT = os.path.join(BASE_DIR)
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
 STATIC_URL = '/static/'
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
