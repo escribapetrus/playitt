131d6877c3d962225f3f5e3383572c422df0636b
escribapetrus <schreiber.mmb@gmail.com>
2020-09-17 13:21:54 -0300

    sfdolfjsd;lf\|

--- _main/settings.py
+++ _main/settings.py
@@ -55,23 +55,24 @@ TEMPLATES = [
 
 WSGI_APPLICATION = '_main.wsgi.application'
 
+# DATABASES = {
+
+# }
+
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pschreiber$playitt',
+        'USER': 'pschreiber',
+        'PASSWORD': 'playitt2304',
+        'HOST': 'pschreiber.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'playitt',
-#         'USER': 'root',
-#         'PASSWORD': '',
-#         'HOST': 'localhost',
-#         'PORT': '3306',
-#     }
-# }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
