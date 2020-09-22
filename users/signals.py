9aa788a21414a3893c2005751eebad315c654fbc
escribapetrus <schreiber.mmb@gmail.com>
2020-09-21 23:52:58 -0300

    make signal for add song to favorites and send emails -- configured gmail stmp

--- users/signals.py
+++ users/signals.py
@@ -1,7 +1,9 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, m2m_changed
 from django.contrib.auth.models import User
 from django.dispatch import receiver
+from django.core.mail import send_mail
 from .models import Profile
+from playlists.models import Playlist
 
 @receiver(post_save, sender=User)
 def create_profile(sender,instance,created,**kwargs):
@@ -11,3 +13,15 @@ def create_profile(sender,instance,created,**kwargs):
 @receiver(post_save, sender=User)
 def save_profile(sender,instance,**kwargs):
     instance.profile.save
+
+@receiver(m2m_changed, sender = Profile.favorites.through)
+def send_email_after_favorite(sender, instance, action, pk_set, **kwargs):
+    if action == "post_add":
+        send_mail(
+            subject="Someone added your playlist to favorites",
+            message="""
+            Hi, {user}! Someone added your playlist {playlist} to their favorites. You're making quite a name for yourself in the music scene. Cheers!
+            """.format(user=instance, playlist=Playlist.objects.get(pk=pk_set.pop())),
+            from_email=None,
+            recipient_list=["playitt.app@gmail.com"]
+        )
